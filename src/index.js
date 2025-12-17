64a7f270a9f59d5841da3972405238aed528e030
AndriyP0 <andreopopovich@gmail.com>
2025-12-17 18:43:05 +0200

    apdate

--- src/index.js
+++ src/index.js
@@ -26,24 +26,24 @@ const sendMessage = (year) => {
     return "Invalid data";
   }
 
-  if (year === 0) {
-    return "The father is twice as old as the son";
-  }
+  switch (true) {
+    case year === 0:
+      return "The father is twice as old as the son";
 
-  if (year === 1) {
-    return "The father will be twice as old as the son in 1 year";
-  }
+    case year === 1:
+      return "The father will be twice as old as the son in 1 year";
 
-  if (year === -1) {
-    return "The father was twice as old as the son 1 year ago";
-  }
+    case year === -1:
+      return "The father was twice as old as the son 1 year ago";
 
-  if (year > 0) {
-    return `The father will be twice as old as the son in ${year} years`;
-  }
+    case year > 0:
+      return `The father will be twice as old as the son in ${year} years`;
 
-  return `The father was twice as old as the son ${Math.abs(year)} years ago`;
+    default:
+      return `The father was twice as old as the son ${Math.abs(year)} years ago`;
+  }
 };
+
 
 const result = getYearDifferent(44, 20);
 console.log(sendMessage(result));
